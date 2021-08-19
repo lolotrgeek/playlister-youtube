e583329fe7ec00bbfe1ceccdbf30f489fffa3c1e
lolotrgeek <jon@simplyenable.com>
2021-08-19 12:42:59 -0500

    initial

---
+++ auth.js
@@ -0,0 +1,155 @@
+var fs = require('fs')
+const path = require('path')
+var readline = require('readline')
+var { google } = require('googleapis')
+var OAuth2 = google.auth.OAuth2
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/youtube']
+var TOKEN_DIR = path.join(__dirname, '/credentials/')
+var TOKEN_PATH = path.join(TOKEN_DIR, 'token.json')
+
+function getCredentials() {
+  return new Promise((resolve, reject) => {
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+      if (err) reject('Error loading client secret file: ' + err)
+
+      // Authorize a client with the loaded credentials, then call the YouTube API.
+      // authorize(JSON.parse(content), getChannel)
+
+      resolve(JSON.parse(content))
+    })
+  })
+
+}
+
+function getToken() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(TOKEN_PATH, function (err, token) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(token))
+      }
+    })
+  })
+}
+
+function getAuthUrl(oauth2Client, callback) {
+  return new Promise((resolve, reject) => {
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    })
+    console.log('Authorize this app by visiting this url: ', authUrl)
+    resolve(authUrl)
+  })
+
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+async function authorize(credentials) {
+  return new Promise((resolve, reject) => {
+    var clientSecret = credentials.installed.client_secret
+    var clientId = credentials.installed.client_id
+    var redirectUrl = credentials.installed.redirect_uris[0]
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl)
+    let token = await getToken()
+    if (token) {
+      oauth2Client.credentials = token
+      resolve({ auth: oauth2Client })
+
+    } else {
+      resolve({ auth: oauth2Client, url: getAuthUrl(oauth2Client) })
+    }
+  })
+}
+
+
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+async function getNewToken(oauth2Client) {
+  return new Promise((resolve, reject) => {
+    oauth2Client.getToken(code, function (err, token) {
+      if (err) {
+        reject('Error while trying to retrieve access token', err)
+        return
+      }
+      oauth2Client.credentials = token
+      await storeToken(token)
+      resolve(oauth2Client)
+    })
+
+  })
+
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.mkdirSync(TOKEN_DIR)
+    } catch (err) {
+      if (err.code != 'EEXIST') {
+        reject(err)
+      }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      if (err) reject(err)
+      console.log('Token stored to ' + TOKEN_PATH)
+      resolve()
+    })
+  })
+
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getChannel(auth) {
+  return new Promise((resolve, reject) => {
+    var service = google.youtube('v3')
+    service.channels.list({
+      auth: auth,
+      part: 'snippet,contentDetails,statistics',
+      forUsername: 'GoogleDevelopers'
+    }, function (err, response) {
+      if (err) {
+        reject('The API returned an error: ' + err)
+      }
+      var channels = response.data.items
+      if (channels.length == 0) {
+        reject('No channel found.')
+      } else {
+        resolve('This channel\'s ID is %s. Its title is \'%s\', and ' +
+          'it has %s views.',
+          channels[0].id,
+          channels[0].snippet.title,
+          channels[0].statistics.viewCount)
+      }
+    })
+  })
+}
+
+module.exports = { getCredentials, authorize, getNewToken, storeToken, getChannel }
---
+++ index.js
@@ -0,0 +1,54 @@
+
+let api_key = "AIzaSyCB3p3A2mcCaI1lVvB0H9HRzY529sH9lzI"
+let base = "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlistItems.update?part=snippet"
+// let list_id = 'PLGZwtzUnUPvi49duFUJApamEUzz2HnSg7'
+
+let token
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 80
+const path = require('path')
+const bodyParser = require('body-parser')
+const { authorize, getChannel, storeToken, getNewToken, getCredentials } = require("./auth")
+
+let credentials
+let client
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/', async (req, res) => {
+    try {
+        if (req.body.auth) {
+            console.log(req.body.auth)
+            credentials = await getCredentials()
+            client = await authorize(credentials)
+            if (client.url) res.redirect(client.url)
+        }
+
+        if (req.body.code) {
+            // successful OAuth2 post
+            console.log(req.body.code)
+            client.auth = await getNewToken(client.auth)
+        }
+
+        if (req.body.playlist) {
+            console.log(req.body.playlist)
+            if (client.auth && client.auth.credentials) {
+                let channel_info = await getChannel(client.auth)
+                res.send(JSON.stringify(channel_info))
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
