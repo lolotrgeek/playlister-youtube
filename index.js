acc4829404538b2e534a97e33d1dff5f64d65469
lolotrgeek <jon@simplyenable.com>
2021-08-19 15:59:25 -0500

    remove linebreaks

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/', async (req, res) => {
 
         if (req.body.playlist && req.body.videos) {
             console.log('playlist:', req.body.playlist)
-            let videoIds = req.body.videos.trim().split(',')
+            let videoIds = req.body.videos.trim().replace(/(\r\n|\n|\r)/gm, "").split(',')
             if (client.auth && client.auth.credentials) {
                 let videosToAdd = videoIds.map(videoId => addVideoToPlaylist(client.auth, playlist, videoId))
                 Promise.allSettled(videosToAdd).then(results => {
