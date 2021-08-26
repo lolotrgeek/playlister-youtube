560d299ec11ff3b30170c3277048044a58c7d6fa
lolotrgeek <jon@simplyenable.com>
2021-08-26 16:47:26 -0500

    add playlist insert

--- src/App.js
+++ src/App.js
@@ -5,21 +5,13 @@ function App() {
   const [auth, setAuth] = useState(null)
 
   useEffect(() => {
-    fetch("http://localhost/api")
-      .then(res => {
-        console.log(res)
-        return res.json()
-      })
-      .then(data => {
-        console.log(data)
-        setAuth(data.message)
-      })
-      .catch(err => console.log(err))
+
+
   }, [])
 
   return (
     <div className="App">
-      <p>{!auth ? "Loading..." : auth}</p>
+
     </div>
   )
 }
